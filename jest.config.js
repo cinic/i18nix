aabd2bc94dcbce57a6050bd9fe82ea75296d29cd
Aleksandr Andreev <cinic.rus@gmail.com>
2019-05-28 18:05:48 +0300

    Moved jest and babel to root dir

--- jest.config.js
+++ jest.config.js
@@ -58,7 +58,11 @@ module.exports = {
   // globalTeardown: null,
 
   // A set of global variables that need to be available in all test environments
-  // globals: {},
+  globals: {
+    'babel-jest': {
+      extends: './babel.config.js'
+    }
+  },
 
   // An array of directory names to be searched recursively up from the requiring module's location
   // moduleDirectories: [
@@ -165,6 +169,9 @@ module.exports = {
 
   // A map from regular expressions to paths to transformers
   // transform: null,
+  transform: {
+    "^.+\\.jsx?$": "babel-jest"
+  },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
