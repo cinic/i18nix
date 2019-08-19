fa761acd6bbe2a9c9a09be44b1485630f24292bd
aandreev <andreev@reksoft.ru>
2019-08-19 18:52:51 +0300

    Added I18n compoennt for text translate

--- packages/react/src/index.test.tsx
+++ packages/react/src/index.test.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react'
 import { shallow } from 'enzyme'
-import { initI18n, setLocale } from 'i18nix'
-import { Translate } from './index'
+import { initI18n, setLocale, t } from 'i18nix'
+import { Translate, I18n } from './index'
 
 const locale = 'en'
-const TRANSLATIONS = { en: { title: 'Nike'}, ru: { title: 'Найк'}}
+const TRANSLATIONS = { en: { title: 'Nike' }, ru: { title: 'Найк' } }
 
 describe('Render component', () => {
   beforeAll(() => initI18n(locale, TRANSLATIONS))
@@ -20,6 +20,15 @@ describe('Render component', () => {
   test('Rerender Translate component', () => {
     const translate = shallow(
       <Translate path={['title']} />,
+    )
+    setLocale('ru')
+
+    expect(translate.text()).toEqual(TRANSLATIONS.ru.title)
+  })
+
+  test('Rerender I18n component', () => {
+    const translate = shallow(
+      <I18n render={() => t('title')} />,
     )
     setLocale('ru')
 
--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,3 +1,4 @@
+import { ReactNode } from 'react'
 import { t, BaseComponent } from 'i18nix'
 
 export class Translate extends BaseComponent<{ path: string[] | string, [key: string]: any }> {
@@ -6,4 +7,8 @@ export class Translate extends BaseComponent<{ path: string[] | string, [key: st
 
     return t(path, otherProps)
   }
+}
+
+export class I18n extends BaseComponent<{ render: () => ReactNode }> {
+  render = this.props.render
 }
