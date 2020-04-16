b8e51cecca7a6a6274e3919fa35e0d4fd0985a9d
Aleksandr Andreev <andreev@reksoft.ru>
2020-04-16 19:28:09 +0300

    feat(core): refactored initial method for hide lib methods.

--- packages/core/src/index.test.ts
+++ packages/core/src/index.test.ts
@@ -1,46 +1,68 @@
-import { initI18n, t, getLocale, setLocale, setTranslations, getTranslations } from './index'
+import { initI18n } from './index'
 import { mergeDeep } from './lib/merge'
 
 const TRANSLATIONS = { en: { title: 'Magazine' }, ru: { title: 'Журнал' } }
-describe('Create instance testing', () => {
+
+describe('i18n instance testing', () => {
+  const { setLocale, addTranslations, t, subscribe } = initI18n('en', TRANSLATIONS)
+
   describe('get nested path', () => {
-    test('expect Magazine', () => {
-      initI18n('en', TRANSLATIONS)
+    test('expect Magazine', () => {  
+      expect(t(['title'])).toBe(TRANSLATIONS.en.title)
+    })
+  })
   
-      expect(t(['title'])).toBe(TRANSLATIONS.en.title);
-    });
+  describe('subscribe setLocale', () => {
+    const subscribers: number[] = []
+    const callback = () => subscribers.push(Math.random())
+
+    test('expect 1 subscribers', () => {
+      subscribe('setLocale', callback)
+      setLocale('ru')
+      expect(subscribers).toHaveLength(1)
+    })
+  })
+
+  describe('subscribe addTranslation', () => {
+    const subscribers: number[] = []
+    const callback = () => subscribers.push(Math.random())
+
+    test('expect 1 subscribers', () => {
+      subscribe('addTranslations', callback)
+      addTranslations({ en: { header: 'Beeer' }})
+      expect(subscribers).toHaveLength(1)
+    })
   })
 })
 
 describe('Locale getter/setter methods', () => {
-  beforeAll(() => {
-    initI18n('en', TRANSLATIONS)
-  })
-
+  const { getLocale, getTranslations, setLocale, addTranslations, t } = initI18n('en', TRANSLATIONS)
+  
   describe('get locale', () => {
     test('expect en', () => {
-      expect(getLocale()).toBe('en');
-    });
+      setLocale('en')
+      expect(getLocale()).toBe('en')
+    })
   })
 
   describe('set locale', () => {
     test('expect ru', () => {
       setLocale('ru')
-      expect(getLocale()).toBe('ru');
-    });
+      expect(getLocale()).toBe('ru')
+    })
   })
 
   describe('use new locale', () => {
     test('expect Журнал', () => {
       setLocale('ru')
-      expect(t(['title'])).toBe(TRANSLATIONS.ru.title);
-    });
+      expect(t(['title'])).toBe(TRANSLATIONS.ru.title)
+    })
   })
   
   describe('add translations', () => {
     test('expect merged translations', () => {
       const translations = { en: { title: 'New Yorker', header: 'Magazine' } }
-      setTranslations(translations)
+      addTranslations(translations)
       expect(getTranslations()).toStrictEqual(mergeDeep(TRANSLATIONS, translations))
     })
   })
